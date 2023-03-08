a1df8648ace474becc2768c7fa10acbd8ec233d0
Matías Vega <elmativega3@gmail.com>
2023-03-08 11:49:36 -0300

    Tipos de datos

--- 2_Tipos_datos/TiposDeDatos.js
+++ 2_Tipos_datos/TiposDeDatos.js
@@ -1,7 +1,10 @@
 //Tipo de dato string
 var nombre="Cande";
-console.log(nombre);
+console.log( typeof nombre);
 nombre=15;
+console.log(typeof nombre);
+nombre='';
+console.log(typeof nombre);
 //uso de TYPEOF
 
 console.log(typeof nombre);
@@ -15,3 +18,43 @@ var objeto={
     telefono:"123455"
 };
 console.log(objeto);
+//Tipos boolean
+let bandera= true;
+console.log(typeof bandera);
+
+// Tipo de dato funcion
+
+function miFn() {  
+}
+console.log(typeof miFn);
+
+//Tipo de dato Symbol
+
+let simbolo= Symbol('mi simbolo');
+console.log(typeof simbolo);
+
+//Tipo de dato, las Clases
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+}
+console.log(typeof Persona);
+
+//Tipo undefined, la tenemos en automatico.
+let x;
+console.log(typeof x);
+// Tipo de Dato null, cambia el typeof.
+let y=null;
+console.log(typeof y);
+
+//Tipo de dato ARRAY es de tipo object
+let autos=["Reanult", "Fiat", "chevrolet"]
+console.log(autos);
+console.log(typeof autos);
+
+
+
+
+
