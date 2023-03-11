1c83a103dc16e7313aa24db78332a9fd91038221
Matías Vega <elmativega3@gmail.com>
2023-03-11 01:17:19 -0300

    inyectar elementos al DOM con JS

---
+++ 22_GenerarHtml_conJS/GenerarHTML_conJS.js
@@ -0,0 +1,80 @@
+// Vamos a ver como crear un enlace
+const enlace=document.createElement('a'); 
+//el createElement crea el elemento que quiero pasar al DOM y entre () especifico lo que quiero, puede ser un DIV, Parrafo y en este caso un Enlace.
+
+enlace.textContent='Sobre Nosotros';//el textContent me permite generar texto.
+
+//Le podemos ir agregando atributos segun sea necesario.
+enlace.href/='/nuevo-enlace';
+enlace.target='_blank';
+enlace.setAttribute('data-atribute','nuevo-enlace');
+// enlace.className('className');
+enlace.classList.add('nave');
+console.log(enlace)
+
+enlace.onclick=unaFuncion;
+
+//Tenemos que definir donde queremos mostrar el elemento.
+//Seleccionar la navegación.
+const navegacion= document.querySelector('.navegacion');
+//agragamos un hijo al NAV
+navegacion.appendChild(enlace);//lo agrega al final
+
+
+//TAMBIEN LO PODEMOS INSETAR DE FORMA CONTROLADA.
+console.log(navegacion.children);//nos da las distintas opsiones para ver donde queremos insertarlo.
+navegacion.insertBefore(enlace,navegacion.children[2]);//lleva dos paramentros 1 lo que queremos insertar, 2 donde.
+
+function unaFuncion(){
+    alert('Diste click');
+}
+
+
+//CREAR UN CARD DE FORMA DINAMICA.
+//paso 1
+const parrafo1=document.createElement('p');
+parrafo1.textContent='Concierto';
+parrafo1.classList.add('categoria', 'concierto');
+
+const parrafo2=document.createElement('p');
+parrafo2.textContent='Musica Infantil';
+parrafo2.classList.add('titulo');
+const parrafo3=document.createElement('p');
+parrafo3.textContent='$1.500';
+parrafo3.classList.add('precio');
+console.log(parrafo1);
+
+//Creo el div que contiene estos parrafos ver clase INFO del index.HTML
+//paso 2
+const info=document.createElement('div');
+info.classList.add('info');
+info.appendChild(parrafo1);
+info.appendChild(parrafo2);
+info.appendChild(parrafo3);
+// es importante el orden de aparicion de los parrafos.
+
+//Crear img
+//paso 3
+const imagen= document.createElement('img');
+imagen.src='../img/Sql.png';
+imagen.alt='text alter';
+
+//Crear el CARD perse
+//paso 4
+const cards= document.createElement('div');
+cards.classList.add('card');
+
+//Asignar la imagen
+cards.appendChild(imagen);
+//paso 5
+
+//Asigno info
+//paso6
+cards.appendChild(info);
+
+
+//INSERTAR EN EL HTML
+//paso 7
+const contenedor= document.querySelector('.hacer .contenedor-Cards');
+console.log(contenedor.children);
+contenedor.insertBefore(cards, contenedor.children[0]);
--- 3_Operadores/5_Fn_isNaN().js
+++ 3_Operadores/5_Fn_isNaN().js
@@ -1,21 +1,14 @@
-// analizamos el 2=(==) y el 3 = (===)
-let a=3, b= 4, c="3";
+let miEdad= "40"; //Para evitar inconsistencias en el codigo lo ponemos como cadena.
+let edad= Number(miEdad);// con la fn Number lo pasamos a número.
+console.log(edad);// en este caso en Not a Number.
+console.log(typeof edad);
 
-let z= a==b;
-console.log(z);
+//Uso de la fn isNaN()
+if(isNaN(edad)){
+    console.log("No es un número");
+}else{
+    let resultado=(edad>=18)?"Puede votar":"Muy joven para votar";
+    console.log(resultado);
+    return resultado
 
-let y= a==c;
-console.log(y);
-// el === tiene en cuenti el tipo de dato
-y= a===c;
-console.log(y);
-console.log(typeof a);
-console.log(typeof c);
-
-// DISTINTO A =  !n
-
-let x = a != c;
-console.log(x);
-
- x = a !== c;
-console.log(x);
+}
